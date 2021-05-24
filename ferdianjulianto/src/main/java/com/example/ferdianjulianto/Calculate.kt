5046711b94dc9df4edbd051e3f53d1840f679958
ivan siregar <ivan.siregar@gmail.com>
2021-05-24 12:06:57 +0700

    first commit

---
+++ ferdianjulianto/src/main/java/com/example/ferdianjulianto/Calculate.kt
@@ -0,0 +1,17 @@
+package com.example.ferdianjulianto
+
+class Calculate {
+
+    companion object
+    {
+        fun kali(i1: Int, i2: Int) : Int {
+            return (i1 * i2)
+        }
+
+        fun tambah(i1: Int, i2: Int) : Int {
+            return (i1 + i2)
+        }
+    }
+
+
+}
